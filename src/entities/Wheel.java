f11e2622f355f814725ad7253d70b6880fd6553b
DGWerlod <ddeanda333@gmail.com>
2020-07-10 16:12:02 -0700

    Built the big boy Wheel
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

--- src/entities/Wheel.java
+++ src/entities/Wheel.java
@@ -1,7 +1,40 @@
 package entities;
 
-public class Wheel {
+import entities.wedges.Wedge;
 
+import java.util.ArrayList;
 
+public class Wheel extends Entity {
+
+    private ArrayList<Wedge> wedges;
+
+    public Wheel(int x, int y, int w, int h) {
+        super(x, y, w, h);
+        this.wedges = new ArrayList<>();
+    }
+
+    public Wheel(int x, int y, int w, int h, ArrayList<Wedge> wedges) {
+        super(x, y, w, h);
+        this.wedges = wedges;
+    }
+
+    public void addWedge(Wedge toAdd) {
+        wedges.add(toAdd);
+    }
+
+    // True if successful, false if not (b/c not present on wheel)
+    public boolean removeWedge(int wedgeID) {
+        for (Wedge w : wedges) {
+            if (w.getID() == wedgeID) {
+                wedges.remove(w);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int spin() {
+        return wedges.get((int)(Math.random() * wedges.size())).getID();
+    }
 
 }
---
+++ src/entities/wedges/Wedge.java
@@ -0,0 +1,10 @@
+package entities.wedges;
+
+public abstract class Wedge {
+
+    public static final int BASIC_WEDGE = 0;
+    //...
+
+    abstract public int getID();
+
+}
