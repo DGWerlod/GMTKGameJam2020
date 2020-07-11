774f17818f9ab6eabfcd680d56dd6e238e72e705
Desmond <desitootoo+github@gmail.com>
2020-07-11 18:18:45 -0500

    Added inital images

--- src/resources/Images.java
+++ src/resources/Images.java
@@ -6,9 +6,15 @@ import processing.core.PImage;
 public class Images {
 
     public static PImage jamLogo;
+    public static PImage maria;
+    public static PImage saturn;
+    public static PImage poof;
 
     public static void loadImages(PApplet display) {
         jamLogo = display.loadImage("img/jamlogo.png");
+        maria = display.loadImage("img/cmyk.png");
+        saturn = display.loadImage("img/saturn.png");
+        poof = display.loadImage("img/poof.png");
     }
 
 }
