3a0a748113e035efafce059af2d89fd395ad6a07
DGWerlod <ddeanda333@gmail.com>
2020-07-12 10:19:28 -0700

    Added game states
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

--- src/entities/actors/heroes/Maria.java
+++ src/entities/actors/heroes/Maria.java
@@ -9,7 +9,7 @@ public class Maria extends Mage {
 
     public Maria(float x, float y) {
         super(x, y, 100,100, 5, 100, 10, new Animation(Images.maria, 8, false, true),
-                new Punch[]{new Punch(x, y, 50, 50, Images.poof, 10,(float)(-Math.PI/2),5)});
+                new Punch[]{new Punch(x, y, 50, 50, Images.poof[0], 10,(float)(-Math.PI/2),5)});
     }
 
     public void moveInDir(float xcomp, float ycomp) {
--- src/graphics/Main.java
+++ src/graphics/Main.java
@@ -8,13 +8,29 @@ import entities.actors.*;
 import entities.Wheel;
 import entities.wedges.*;
 
+import static graphics.GAME_STATE.*;
+
+enum GAME_STATE {
+    AUTHORS,
+    JAM_LOGO,
+    SPLASH,
+    SELECTION,
+    ENCOUNTER,
+    VICTORY,
+    DEFEAT,
+    PAUSE
+}
+
 public class Main extends PApplet {
 
-    Maria maria;
-    Enemy saturn;
-    Wheel wheel;
+    private Maria maria;
+    private Enemy saturn;
+    private Wheel wheel;
 
-    boolean clicking = false;
+    private boolean mouseClicked = false;
+    private boolean mouseHeld = false;
+
+    private GAME_STATE currentState;
 
     public void settings() {
         size(1440, 800);
@@ -24,42 +40,80 @@ public class Main extends PApplet {
         imageMode(CENTER);
         Images.loadImages(this);
         textFont(createFont("fonts/muli.ttf", 32));
-        maria = new Maria(width/2, height/2);
+        maria = new Maria((float) width / 2, (float) height / 2);
         saturn = new Saturn(random(width), random(height));
         wheel = new Wheel(0, 0, 50, 50);
         wheel.addWedge(new BasicWedge());
         wheel.addWedge(new StarWedge());
+        currentState = AUTHORS;
     }
 
     public void draw() {
-        image(Images.background, width/2, height/2, width, height);
+        image(Images.background, (float) width / 2, (float) height / 2, width, height);
+        switch (currentState) {
+            case AUTHORS:
+                text("Hello", 10, 30);
+                if (mouseClicked) {
+                    currentState = JAM_LOGO;
+                }
+                break;
+            case JAM_LOGO:
+                image(Images.jamLogo, (float) width / 2, (float) height / 2, width, height);
+                if (mouseClicked) {
+                    currentState = SPLASH;
+                }
+                break;
+            case SPLASH:
+                text("This is the splash screen my dude", 10, 30);
+                if (mouseClicked) {
+                    currentState = SELECTION;
+                }
+                break;
+            case SELECTION:
+                currentState = ENCOUNTER;
+                break;
+            case ENCOUNTER:
+                maria.draw(this);
+                maria.go(this);
+                maria.act(new Actor[]{saturn});
+                if (Math.abs(mouseX - maria.getX()) > 5 || Math.abs(mouseY - maria.getY()) > 5) {
+                    maria.moveInDir(mouseX - maria.getX(), mouseY - maria.getY());
+                }
 
-        maria.draw(this);
-        maria.go(this);
-        maria.act(new Actor[]{saturn});
-        if (Math.abs(mouseX - maria.getX()) > 5 || Math.abs(mouseY - maria.getY()) > 5) {
-            maria.moveInDir(mouseX - maria.getX(), mouseY - maria.getY());
+                saturn.draw(this);
+                saturn.go(this);
+                saturn.act(new Actor[]{maria});
+
+                if (mouseHeld) {
+                    wheel.translate(mouseX, mouseY);
+                    wheel.draw(this);
+                }
+                break;
+            case VICTORY:
+                break;
+            case DEFEAT:
+                break;
+            case PAUSE:
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + currentState);
         }
 
-        saturn.draw(this);
-        saturn.go(this);
-        saturn.act(new Actor[]{maria});
+        // reset mouseClicked
+        mouseClicked = false;
 
-        if (clicking) {
-            wheel.translate(mouseX, mouseY);
-            wheel.draw(this);
-        }
-
-        text("Hello", 10, 30);
     }
 
     public void mousePressed() {
-        clicking = true;
-
+        mouseHeld = true;
     }
 
     public void mouseReleased() {
-        clicking = false;
+        mouseHeld = false;
+    }
+
+    public void mouseClicked() {
+        mouseClicked = true;
     }
 
     public static void main(String[] args) {
